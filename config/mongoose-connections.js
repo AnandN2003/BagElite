cb6f62835d0e9783e9fda0dac52f9ba24cb70cf1
Anand Pranav <anand.nijagal@gmail.com>
2025-01-09 09:36:09 +0530

    owner creation in route and checking in postman , setting up NODE_ENV as development

--- config/mongoose-connections.js
+++ config/mongoose-connections.js
@@ -1,12 +1,14 @@
 const mongoose = require("mongoose");
+const config = require("config");
+const dbgr = require("debug")("development:mongoose")
 
 mongoose
-.connect("mongodb://localhost:27017/BagElite")
+.connect(`${config.get("MONGODB_URI")}/BagElite`)
 .then(()=>{
-    console.log("connected");
+    dbgr("connected");
 })
 .catch((err)=>{
-    console.log(err);
+    dbgr(err);
 });
 
 module.exports = mongoose.connection;
--- routes/ownersRouter.js
+++ routes/ownersRouter.js
@@ -1,8 +1,41 @@
 const express = require("express");
 const router = express.Router();
+const ownerModel = require("../models/owner-model");
+
+console.log("NODE_ENV:", process.env.NODE_ENV);
 
 router.get("/",(req,res)=>{
     res.send("hey what is up!");
 });
+
+router.delete("/deleteAll", async (req, res) => {
+    try {
+      await ownerModel.deleteMany(); // Deletes all documents in the collection
+      res.send("All owners have been deleted.");
+    } catch (error) {
+      res.status(500).send("Error deleting owners: " + error.message);
+    }
+  });
+  
+  
+
+if(process.env.NODE_ENV === "development"){
+    router.post("/create",async (req,res)=>{
+        let owners = await ownerModel.find();
+        if(owners.length>0){
+            return res.status(503).send("You don't have permisssion to create a new owner.");
+        }
+
+        let {fullname,email,password} = req.body;
+
+        let createdOwner = await ownerModel.create({
+            fullname : fullname,
+            email : email,
+            pssword : password,
+        });
+        res.status(201).send(createdOwner)
+    });
+}
+
 
 module.exports = router;
